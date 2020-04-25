1bbd254c4486cc42c7aeba6a4f071649814ea8ef
Gagan Malvi <malvigagan@gmail.com>
2020-04-25 10:51:43 +0000

    Clear out build tools mistakenly added due to not updating .gitignore
    Updated .gitignore too! :)

--- out/include/generated/compile.h
+++
@@ -1,7 +0,0 @@
-/* This file is auto generated, version 0 */
-/* SMP PREEMPT */
-#define UTS_MACHINE "aarch64"
-#define UTS_VERSION "#0 SMP PREEMPT Sat Apr 25 10:22:43 UTC 2020"
-#define LINUX_COMPILE_BY "gagan"
-#define LINUX_COMPILE_HOST "triton"
-#define LINUX_COMPILER "gcc version 4.9.x 20150123 (prerelease) (GCC)"
--- out/include/generated/uapi/linux/version.h
+++
@@ -1,2 +0,0 @@
-#define LINUX_VERSION_CODE 264665
-#define KERNEL_VERSION(a,b,c) (((a) << 16) + ((b) << 8) + (c))
